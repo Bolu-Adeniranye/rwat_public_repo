e70789e40565fe206bec01b068bf730f171f8453
Bolu Adeniranye <68526923+Bolu-Adeniranye@users.noreply.github.com>
2025-12-14 17:18:12 +0000

    Final checks for any bugs, seems to be working fine

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
 import "./shapecard.js";
 import "./memory-game.js";
+
+// Removed all code related to the MemoryGame class and its functionality.
+// Code now only imports the necessary modules without defining or using the MemoryGame class.
